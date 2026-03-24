8b4d04acb2afda2a61521061e221d044404fa526
SLevendeev <stanislav-levendeev@mail.ru>
2026-03-24 09:37:29 +0100

    Add additional weight files for metric video depth models

--- get_weights.sh
+++ get_weights.sh
@@ -4,3 +4,7 @@ mkdir checkpoints
 cd checkpoints
 wget https://huggingface.co/depth-anything/Video-Depth-Anything-Small/resolve/main/video_depth_anything_vits.pth
 wget https://huggingface.co/depth-anything/Video-Depth-Anything-Large/resolve/main/video_depth_anything_vitl.pth
+wget https://huggingface.co/depth-anything/Metric-Video-Depth-Anything-Large/resolve/main/metric_video_depth_anything_vitl.pth
+wget https://huggingface.co/depth-anything/Metric-Video-Depth-Anything-Small/resolve/main/metric_video_depth_anything_vits.pth
+wget https://huggingface.co/depth-anything/Video-Depth-Anything-Base/resolve/main/video_depth_anything_vitb.pth
+wget https://huggingface.co/depth-anything/Metric-Video-Depth-Anything-Base/resolve/main/metric_video_depth_anything_vitb.pth
